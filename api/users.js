7e79f07ac8a45964c85a6e58e9c810911dd286a8
Nematov Ro'zimuhammad <jeki2004114r@gmail.com>
2024-08-27 02:14:44 +0500

    amin

--- api/users.js
+++ api/users.js
@@ -27,7 +27,19 @@ app.use(cors()); // CORSni yoqish
 
 app.use(express.json());
 
-let users = [];
+let users = [
+    { first_name: "Jeki", last_name: "Tommy", age: 20, hobbies: "pianino" },
+    { first_name: "Aisha", last_name: "Ali", age: 25, hobbies: "yog'och o'yish" },
+    { first_name: "Omar", last_name: "Khan", age: 30, hobbies: "fotografiya" },
+    { first_name: "Mariya", last_name: "Ivanova", age: 22, hobbies: "chizmachilik" },
+    { first_name: "Samir", last_name: "Hussein", age: 28, hobbies: "velosiped minish" },
+    { first_name: "Fatima", last_name: "Ahmed", age: 27, hobbies: "taom tayyorlash" },
+    { first_name: "Daniel", last_name: "Smith", age: 32, hobbies: "badiiy adabiyot" },
+    { first_name: "Elena", last_name: "Petrova", age: 24, hobbies: "sayohat qilish" },
+    { first_name: "Ahmed", last_name: "Aliyev", age: 29, hobbies: "fitness" },
+    { first_name: "Nargiza", last_name: "Yusupova", age: 26, hobbies: "pishiriqlar tayyorlash" }
+];
+
 
 app.get('/api/users', (req, res) => {
     res.json(users);
