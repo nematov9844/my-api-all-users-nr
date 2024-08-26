ee5effc27e49db406caf313a4f22dc1fdcba660f
Nematov Ro'zimuhammad <jeki2004114r@gmail.com>
2024-08-27 01:35:27 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+let users = [];
+
+app.get('/api/users', (req, res) => {
+    res.json(users);
+});
+
+app.post('/api/users', (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.status(201).json(user);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
